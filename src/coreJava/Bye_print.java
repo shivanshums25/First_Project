b79eed9f00e6495ec9dcbc21fba6d83bc7c5803d
Shivanshu <shivanshums25@gmail.com>
2020-05-22 16:41:56 +0530

    Commiting BranchX code

---
+++ src/coreJava/Bye_print.java
@@ -0,0 +1,14 @@
+package coreJava;
+
+/**
+ * @author Shivanshu
+ *
+ */
+public class Bye_print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bye bye");
+	}
+
+}
